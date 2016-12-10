a14eb581caff0123a0418ca9895e87d035aa0c7e
ferdude <fardeenmohdpl@gmail.com>
2016-12-11 00:18:25 +0100

    COME FIND ME

--- app/src/main/java/pl/edu/pw/student/mini/gasstation/MapViewFragment.java
+++ app/src/main/java/pl/edu/pw/student/mini/gasstation/MapViewFragment.java
@@ -6,13 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -42,6 +39,7 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
             e.printStackTrace();
         }
 
+        //I WAS HERE $$420$$
         /*
         // latitude and longitude
         double latitude = 17.385044;
